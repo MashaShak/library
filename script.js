44508234d134705c551eccdcfad202ebed86e058
“gagamot” <shakurovainfo@gmail.com>
2020-09-18 23:34:51 -0400

    Add function to add a book in array and local storage

--- script.js
+++ script.js
@@ -1,46 +1,40 @@
 const addForm = document.querySelector("#add-book");
-    
+const display = document.querySelector("#display");
+const tableBooks = document.querySelector("table");
+let mylibrary = [];
+/*    
 const book1 = {
 title: "War and Peace",
 author: "Leo Tolstoy",
 pages: 569,
 status: "read",
 }
-
 const book2 = {
     title: "Little Prince",
     author: "Antoine de Saint-Exupéry",
     pages: 234,
     status: "not read",
 }
-
 const book3 = {
     title: "Little Prince",
     author: "Antoine de Saint-Exupéry",
     pages: 234,
     status: " not read",
 }
-
 const book4 = {
     title: "Little Prince",
     author: "Antoine de Saint-Exupéry",
     pages: 234,
     status: "read",
 }
-
 let mylibrary = [book1, book2, book3, book4];
+*/
 
-displayBooks();
-displayListOfBooks();
+
 
 
 function showForm() {
     addForm.style.display = (addForm.style.display == "none") ? "block" : "none";
-    
-    
-    //if (addBookForm.style.display == "none"): 
-    //    addBookForm.style.display = "block"?"none";
-    //} else addBookForm.style.display = "none";
 }
 
 function Book(title, author, pages, status){
@@ -49,9 +43,8 @@ function Book(title, author, pages, status){
     this.pages = pages 
     this.isRead = status
 }
-//display all books, go through the array and show the book in the list from the end of the array
+/*display all books, go through the array and show the book in the list from the end of the array
 function displayBooks(){
-    const display = document.querySelector("#display");
     mylibrary.forEach(book => {
         let divBook = document.createElement("div"); 
         const btnDeleteBook = document.createElement("button");
@@ -64,12 +57,11 @@ function displayBooks(){
             divBook.appendChild(divKey);
         }
         divBook.appendChild(btnDeleteBook);
-       
         })
 }
 function displayListOfBooks() {
     mylibrary.forEach(book => { 
-        let tableBooks = document.querySelector("table");
+        
         let trBook = document.createElement("tr");
         tableBooks.appendChild(trBook);
         const btnDeleteBook = document.createElement("button");
@@ -83,19 +75,57 @@ function displayListOfBooks() {
         
         })
 }
+*/
 
-function addBookToLibrary() {
-    //add user input to library
+function displayBook(book) {
+    let divBook = document.createElement("div"); 
+    divBook.classList.add("card");
+    const btnDeleteBook = document.createElement("button");
+    btnDeleteBook.innerHTML = "<img src='https://img.icons8.com/ios/30/000000/delete-forever.png'/>";
+    display.appendChild(divBook);
+    for (let key in book) {
+            let divKey = document.createElement("div");
+            divKey.textContent = book[key];
+            divBook.appendChild(divKey);
+        }
+    divBook.appendChild(btnDeleteBook);
+}
+
+function displayListOfBooks(book) {
+    let trBook = document.createElement("tr");
+    tableBooks.appendChild(trBook);        
+    const btnDeleteBook = document.createElement("button");
+    btnDeleteBook.innerHTML = "<img src='https://img.icons8.com/ios/30/000000/delete-forever.png'/>";
+    for (let key in book) {
+        const tdBook = document.createElement("td");
+        tdBook.textContent = book[key];
+        trBook.appendChild(tdBook);
     }
-function addNewBook() {
-    //make a form to enter the book, add it to mylibrary and display it
+    trBook.appendChild(btnDeleteBook);
 }
 
-function isRead() {
-//change book status read/unread
+
+const addNewBook = (e) => {
+    e.preventDefault(); //prevent form from submitting
+    
+    let newBook = new Book(
+        document.querySelector("#title").value,
+        document.querySelector("#author").value,
+        document.querySelector("#pages").value,
+        document.querySelector("#status").value
+    );
+    mylibrary.push(newBook);
+    console.log(mylibrary);
+    addForm.reset();
+    displayBook(newBook);
+    displayListOfBooks(newBook);
+    
+    //local storage
+    localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
 }
 
-function deleteBook() {
+document.addEventListener("DOMContentLoaded", ()=> {
+    document.querySelector("#addNewBook").addEventListener("click", addNewBook);
+});
 
-}
 
