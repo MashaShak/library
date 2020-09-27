26793a8cfb1cc907834a05faea8e19c10d9dd0f0
“gagamot” <shakurovainfo@gmail.com>
2020-09-26 20:11:33 -0400

    refactor code. split DOM manipulation and event function on buttons.

--- script.js
+++ script.js
@@ -5,12 +5,17 @@ const tbody = document.querySelector("tbody");
 
 let mylibrary = localStorage.getItem("MyLibrary") ? JSON.parse(localStorage.getItem("MyLibrary")):[];
 
-localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
-const data = JSON.parse(localStorage.getItem("MyLibrary"));
-data.forEach((book) => {
-        displayAll(display, "div", "div", book); 
-});
+saveToStorage();
+uploadFromStorage();
 
+function uploadFromStorage(parent = display, element = "div", child = "div") {
+    const data = JSON.parse(localStorage.getItem("MyLibrary"));
+    data.forEach((book) => displayBooks(parent, element, child, book));
+}
+
+function saveToStorage() {
+    localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
+}
 
 const showForm = () => form.style.display = (form.style.display == "block") ? "none" : "block";
 
@@ -21,8 +26,41 @@ function Book(title, author, pages, status){
     this.pages = pages 
     this.isRead = status
 }
+function deleteBook() {
+    const parent = this.parentElement;
+    const index = mylibrary.findIndex(item => item.id == parseFloat(parent.id));
+    mylibrary.splice(index, 1);
+    saveToStorage();
+    parent.remove();
+    countBooks();
+}
+function btnDeleteBook() {
+    const button = document.createElement("button");
+    button.classList.add("btn-icons");
+    button.innerHTML = '<i class="material-icons">delete</i>'; 
+    button.addEventListener('click', deleteBook);
+    return button;
+}
 
-function displayAll(parent, element, elementChild, book) {
+function changeStatus() {
+    let status = this.previousSibling;
+    if (status.innerHTML == "Read") status.innerHTML = "In Progress";
+    else if (status.innerHTML == "In Progress") status.innerHTML = "Not Read";
+    else status.innerHTML = "Read";
+    let index = mylibrary.findIndex(item => item.id == parseFloat(this.parentElement.id));  
+    mylibrary[index]["isRead"] = status.innerHTML;
+    saveToStorage();
+    countBooks();
+}
+function btnChangeStatus() {
+    const button = document.createElement("button");
+    button.innerHTML = "Change Status";
+    button.classList.add("btn-style", "btn-status");
+    button.addEventListener('click', changeStatus);
+    return button;
+}
+
+function displayBooks(parent = display, element = "div", child = "div", book) {
     const el = document.createElement(element);
     parent.appendChild(el);    
     if (parent == display) el.classList.add("card");
@@ -30,52 +68,24 @@ function displayAll(parent, element, elementChild, book) {
         if (key == "id") {
             el.id = book[key];
         } else { 
-        const elChild = document.createElement(elementChild);
+        const elChild = document.createElement(child);
         if (key == "author") elChild.textContent = "by ";
         elChild.textContent += book[key];
         if (key == "pages") elChild.textContent += " pages";
         el.appendChild(elChild);
         }
     }
-    el.appendChild(changeStatus());
-    el.appendChild(deleteBook());
+    el.appendChild(btnChangeStatus());
+    el.appendChild(btnDeleteBook());
 }
 
 function countBooks() {
     let read = mylibrary.filter(item => item.isRead == "Not Read").length;
     let inProgress = mylibrary.filter(item => item.isRead == "In Progress").length;
     let finished = mylibrary.filter(item => item.isRead == "Read").length;
-    document.querySelector("#all").innerHTML = `${read} Not Read  &middot;  ${inProgress} In Progress  &middot;  ${finished} Finishied`; 
+    document.querySelector("#all").innerHTML = `${read} Not Read  &middot;  ${inProgress} In Progress  &middot;  ${finished} Finished`; 
 }
-function deleteBook() {
-    const button = document.createElement("button");
-    button.classList.add("btn-icons");
-    button.innerHTML = '<i class="material-icons">delete</i>'; 
-    button.addEventListener('click', function() {
-        let index = mylibrary.findIndex(item => item.id == parseFloat(button.parentElement.id));
-        mylibrary.splice(index, 1);
-        localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
-        button.parentElement.remove();
-        countBooks();
-    });
-    return button;
-}
-function changeStatus() {
-    const button = document.createElement("button");
-    button.innerHTML = "Change Status";
-    button.classList.add("btn-style", "btn-status");
-    button.addEventListener('click', function() {
-        let status = button.previousSibling;
-        if (status.innerHTML == "Read") status.innerHTML = "In Progress";
-        else if (status.innerHTML == "In Progress") status.innerHTML = "Not Read";
-        else status.innerHTML = "Read";
-        let index = mylibrary.findIndex(item => item.id == parseFloat(button.parentElement.id));
-        mylibrary[index]["isRead"] = status.innerHTML;
-        localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
-        countBooks();
-    })
-    return button;
-}
+
 const addNewBook = (e) => {
     e.preventDefault(); 
     let newBook = new Book(
@@ -85,9 +95,9 @@ const addNewBook = (e) => {
         document.querySelector("#status").value
     );
     mylibrary.push(newBook);
-    localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
-    displayAll(display, "div", "div", newBook); 
-    displayAll(tbody, "tr", "td", newBook);
+    saveToStorage();
+    displayBooks(); 
+    displayBooks(tbody, "tr", "td", newBook);
     countBooks();
     form.reset();
 }
@@ -101,10 +111,7 @@ function showGrid() {
         list.style.display = "none";    
         display.style.display = "block";
         clear(display);
-        mylibrary.forEach((book) => {
-            displayAll(display, "div", "div", book); 
-        });
-
+        uploadFromStorage();
     }
 }
 function showList() {
@@ -112,9 +119,7 @@ function showList() {
         display.style.display = "none";
         list.style.display = "block"; 
         clear(tbody);
-        mylibrary.forEach((book) => {
-            displayAll(tbody, "tr", "td", book);   
-        });
+        uploadFromStorage(tbody, "tr", "td");
     }
 }
 
