547759ff236abc038b301190256c68cd99823acd
“gagamot” <shakurovainfo@gmail.com>
2020-09-28 11:16:32 -0400

    style update

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const form = document.querySelector("form");
 const display = document.querySelector("#display");
 const list = document.querySelector("#list");
 const tbody = document.querySelector("tbody");
+const btnShowForm = document.querySelector("#show-form");
+    
 
 let mylibrary = localStorage.getItem("MyLibrary") ? JSON.parse(localStorage.getItem("MyLibrary")):[];
 
@@ -17,8 +19,20 @@ function saveToStorage() {
     localStorage.setItem("MyLibrary", JSON.stringify(mylibrary));
 }
 
-const showForm = () => form.style.display = (form.style.display == "block") ? "none" : "block";
-
+const showForm = () => {
+    form.style.display = "block";
+    btnShowForm.style.display = "none";
+}
+const hideForm = () => {
+    form.style.display = "none";
+    btnShowForm.style.display = "block";
+}
+const startLibrary = () => {
+    const main = document.querySelector(".main");
+    main.style.display = "block";
+    const intro = document.querySelector(".intro");
+    intro.style.display = "none";
+}
 function Book(title, author, pages, status){
     this.id = Date.now()
     this.title = title
@@ -55,7 +69,7 @@ function changeStatus() {
 function btnChangeStatus() {
     const button = document.createElement("button");
     button.innerHTML = "Change Status";
-    button.classList.add("btn-style", "btn-status");
+    button.classList.add("btn", "btn-status");
     button.addEventListener('click', changeStatus);
     return button;
 }
@@ -83,11 +97,12 @@ function countBooks() {
     let read = mylibrary.filter(item => item.isRead == "Not Read").length;
     let inProgress = mylibrary.filter(item => item.isRead == "In Progress").length;
     let finished = mylibrary.filter(item => item.isRead == "Read").length;
-    document.querySelector("#all").innerHTML = `${read} Not Read  &middot;  ${inProgress} In Progress  &middot;  ${finished} Finished`; 
+    document.querySelector("#book-count").innerHTML = `${read} Not Read  &middot;  ${inProgress} In Progress  &middot;  ${finished} Finished`; 
 }
 
 const addNewBook = (e) => {
     e.preventDefault(); 
+    //need to add validation here 
     let newBook = new Book(
         document.querySelector("#title").value,
         document.querySelector("#author").value,
